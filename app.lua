57afec178e3ddeee77fc98b2d86b43c6e25f1249
Sabbir Ahmed <sabbiirr@gmail.com>
2016-04-18 19:58:48 +0600

    Update app.lua

--- app.lua
+++ app.lua
@@ -1,17 +1,17 @@
 local events = require 'events'
 local parser = require 'parser'
 local server = require 'server'
-local json = require 'cjson'
+local json   = require 'cjson'
 
 
-local App = function(ws)
+local App = function(ws) -- main handler
   -- Create a client
   local client = {}
   client.send = function(data) ws:send(json.encode(data)) end -- send function
   while true do -- loop
     local body, code = ws:receive()
     if body then
-      client.data = parser.parse(body) -- parse icoming message
+      client.data = parser.parse(body) -- parse incoming message
       events.listen(client) -- event Listeners
     else
       ws:close() -- Close connection
