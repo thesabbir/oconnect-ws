7f5b4c66b4381f52e5eb682ec37f9dc6fbd32854
Sabbir Ahmed <sabbiirr@gmail.com>
2016-04-18 20:00:14 +0600

    Update events.lua

--- events.lua
+++ events.lua
@@ -1,22 +1,26 @@
 local json = require 'cjson'
 
 -- Event Listeners
+-- Currently on Echo
 
 local events = {}
 
+-- Error Event
 events['error'] = function(client)
   client.send(client.data)
 end
 
+-- Status Event
 events['status'] = function(client)
   client.send(client.data)
 end
 
+-- Get Info Event
 events['getinfo'] = function(client)
   client.send(client.data)
 end
 
--- dispatcher
+-- Event Dispatcher
 local function eventListeners(client)
   print(client.data.event)
   events[client.data.event](client)
